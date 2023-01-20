c7070a724043185f5c560c8fdade5a2e401fb264
alberto <albertocandido98@gmail.com>
2023-01-19 21:25:50 -0300

    finaliza exercicios adicionando 5 e 6

--- exerc/src/index.ts
+++ exerc/src/index.ts
@@ -1,8 +1,40 @@
+import sequelize from 'sequelize';
 import Author from './database/models/AuthorModel';
+import Book from './database/models/BookModel';
+
+
+// Teste 4 - Author Model
+
+// (async () => {
+//   const authors = await Author.findAll();
+//   console.table(authors.map((author) => author.toJSON()));
+
+//   process.exit(0);
+// })();
+
+
+// Teste 5 - Model Book
+// (async () => {
+//   const books = await Book.findAll();
+//   console.table(books.map((book) => book.toJSON()));
+
+//   process.exit(0);
+// })();
 
 (async () => {
-  const authors = await Author.findAll();
-  console.table(authors.map((author) => author.toJSON()));
+  const data = await Author.findAll({
+    include: {
+      model: Book,
+      attributes: [],
+    },
+    attributes: [
+      ['name', 'author'],
+      [sequelize.fn('COUNT', sequelize.col('books.id')), 'totalBooks']
+    ],
+    group: 'authors.name',
+    raw: true,
+  });
 
+  console.log(data);
   process.exit(0);
 })();
